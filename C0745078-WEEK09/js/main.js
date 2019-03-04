09a5e5747cb9edb74e9864c70d4da39a6a31f0b2
jestin skaria <jestinskaria15@gmail.com>
2019-03-04 16:13:48 -0500

    Commit 4.15 pm

--- C0745078-WEEK09/js/main.js
+++ C0745078-WEEK09/js/main.js
@@ -1,1 +1,19 @@
 ﻿// Your code here!
+
+var addNumbers = {
+    startNumber: 1109,
+    endNumber: 119,
+    sum: 0,
+    adder: function () {
+
+        for (i = this.startNumber; i >= this.endNumber;) {
+            this.sum = this.sum + i;
+            i = i - 11;
+
+        }
+        console.log(this.sum);
+        return this.sum;
+
+    }
+
+};
